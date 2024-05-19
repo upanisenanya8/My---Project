eea2c12d46da2b8bec5b5bff70875acb28a194d7
DNWijendra <dinithiw01@gmail.com>
2024-05-19 10:48:44 +0530

    Implementing the total number of words  and the frequency of words in the text

--- text_processing.py
+++ text_processing.py
@@ -4,3 +4,19 @@ def count_words(text):
     print(f'The text contains {num_words} words.')
 
 count_words("Hello, this is a demonstration text")
+
+def count_words(text):
+    words = text.split()
+    num_words = len(words)
+    word_frequency = {}
+
+    for word in words:
+        if word in word_frequency:
+            word_frequency[word] += 1
+        else:
+            word_frequency[word] = 1
+
+    print(f'The text contains {num_words} words.')
+    print(f'Word frequencies: {word_frequency}')
+
+count_words("Hello, this is a demonstration text.")
